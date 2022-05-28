4d9af9145dd0877f9dd421e8392ebfa011cb7be3
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-05-28 18:17:54 +0530

    create hard coded array of words

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ const scoreDisplay = document.querySelector("#score");
 const timeDisplay = document.querySelector("#time");
 const message = document.querySelector("#message");
 const seconds = document.querySelector("#seconds");
+
+const words = [
+    'hat',
+    'river',
+    'lucky',
+    'statue',
+    'generate',
+    'stubborn',
+    'cocktail',
+    'runaway',
+    'joke',
+    'developer',
+    'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'definition'
+];
