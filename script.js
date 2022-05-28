36ef98d0e6590e526551fdc40e348fe0c327a8ba
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-05-28 18:14:57 +0530

    create DOM elements variables

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function preLoader() {
 let time = 5;
 let score = 0;
 let isPlaying;
+
+// DOM ELEMENTS VARIABLES
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
