ef4b4a4d5525d6fff2416ab45412f4cc21888cdc
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-05-28 17:54:13 +0530

    Create global variables

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// PreLoader
+// PRELOADER
 var loader = document.getElementById("loading");
 function preLoader() {
     loader.style.display = "none";
 }
+
+// GLOBAL VARIABLES
+let time = 5;
+let score = 0;
+let isPlaying;
